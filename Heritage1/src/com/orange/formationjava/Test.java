1a0953a7d827847dea5498fbffd822207a2fd0b4
Philippe LEMORDANT <philippe.lemordant@orange.com>
2016-11-25 12:30:04 +0100

    Add solution to Heritage1

---
+++ Heritage1/src/com/orange/formationjava/Test.java
@@ -0,0 +1,22 @@
+package com.orange.formationjava;
+
+public class Test {
+
+	Animal[] animaux;
+
+	public void faireCrierTousLesAnimaux() {
+		animaux = new Animal[3];
+		animaux[0] = new Cow("Marguerite");
+		animaux[1] = new Horse("Tornado");
+		animaux[2] = new Sheep("Dolly");
+		for (Animal animal : animaux) {
+			System.out.println("L\'animal '" + animal.getName() + "' du type '" + animal.getClass().getSimpleName()
+					+ "' a pour cri: " + animal.sound());
+		}
+	}
+
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.faireCrierTousLesAnimaux();
+	}
+}
