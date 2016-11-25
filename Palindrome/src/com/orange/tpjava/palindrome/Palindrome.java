b3a7278acb0db3d4df5e6a26779d41ee40549c9a
Philippe LEMORDANT <philippe.lemordant@orange.com>
2016-11-25 09:19:05 +0100

    Manage null pointer

--- Palindrome/src/com/orange/tpjava/palindrome/Palindrome.java
+++ Palindrome/src/com/orange/tpjava/palindrome/Palindrome.java
@@ -3,6 +3,9 @@ package com.orange.tpjava.palindrome;
 public class Palindrome {
 
 	static boolean estPalindrome(String s) {
+		if (s == null) {
+			throw new NullPointerException("Invalid null parameter");
+		}
 		for (int i = 0; i < s.length() / 2; i++) {
 			if (s.charAt(i) != s.charAt(s.length() - i - 1))
 				return false;
