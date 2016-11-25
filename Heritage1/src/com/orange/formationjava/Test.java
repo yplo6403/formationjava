80c3247348ee5cfeaa1be8d848086f4b2cc0f192
Philippe LEMORDANT <philippe.lemordant@orange.com>
2016-11-25 13:47:28 +0100

    Ajout classe Test pour faire crier tous les animaux

---
+++ Heritage1/src/com/orange/formationjava/Test.java
@@ -0,0 +1,15 @@
+package com.orange.formationjava;
+
+public class Test {
+
+	Animal[] animaux;
+
+	public void faireCrierTousLesAnimaux() {
+		// A compléter
+	}
+
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.faireCrierTousLesAnimaux();
+	}
+}
