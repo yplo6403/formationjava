0108e3e44aa669399966fb6bcc70f75508dd7ced
Philippe LEMORDANT <Philippe LEMORDANT>
2016-11-25 08:43:47 +0100

    add palindrome

---
+++ Palindrome/src/com/orange/tpjava/palindrome/Palindrome.java
@@ -0,0 +1,19 @@
+package com.orange.tpjava.palindrome;
+
+public class Palindrome {
+
+	static boolean estPalindrome(String s) {
+		for (int i = 0; i < s.length() / 2; i++) {
+			if (s.charAt(i) != s.charAt(s.length() - i - 1))
+				return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			String mot = args[0];
+			System.out.println(mot + " ? " + estPalindrome(mot));
+		}
+	}
+}
