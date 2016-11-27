bcbba6a25f7b61521e4541428d411eadda410f8f
Philippe LEMORDANT <Philippe LEMORDANT>
2016-11-27 16:44:14 +0100

    Shuffle age of persons

--- Collection/src/com/orange/formationjava/trier/list/CustomSortList.java
+++ Collection/src/com/orange/formationjava/trier/list/CustomSortList.java
@@ -18,16 +18,14 @@ public class CustomSortList<E> {
 	public static void main(String[] args) {
 		CustomSortList<Person> test = new CustomSortList<>();
 		List<Person> myList = new ArrayList<>();
-		myList.add(new Person("Bruno", "FROGER", 1));
-		myList.add(new Person("Patrice", "WACRENIER", 2));
-		myList.add(new Person("Christophe", "BETOULE", 3));
-		myList.add(new Person("Oliver", "RESNAIS", 4));
-		myList.add(new Person("Jerome", "CRESTEL", 5));
-		myList.add(new Person("Thierry", "MARCOT", 6));
-		test.display(myList);
+		myList.add(new Person("Bruno", "FROGER", 3));
+		myList.add(new Person("Patrice", "WACRENIER", 4));
+		myList.add(new Person("Christophe", "BETOULE", 1));
+		myList.add(new Person("Oliver", "RESNAIS", 2));
+		myList.add(new Person("Jerome", "CRESTEL", 6));
+		myList.add(new Person("Thierry", "MARCOT", 5));
 		// Afficher une liste triée par age
-		Collections.sort(myList, new Comparator<Person>() {
-			
-		});
+
+		test.display(myList);
 	}
 }
