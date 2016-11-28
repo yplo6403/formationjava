9992f3a044f2ed159b66e2d158131a4fb63f1c7b
Philippe LEMORDANT <Philippe LEMORDANT>
2016-11-28 20:27:11 +0100

    Add Tableau project

---
+++ Tableau/src/com/orange/formationjava/tableau/Tableau.java
@@ -0,0 +1,21 @@
+package com.orange.formationjava.tableau;
+
+import java.util.Arrays;
+
+public class Tableau {
+	
+	public static void main(String[] args) {
+		// Tableau de types primitifs
+		int[] tableauInt;
+		tableauInt = new int[3];
+		tableauInt[0] = 1;
+		tableauInt[1] = 2;
+		tableauInt[2] = 3;
+		
+		// Tableau de types objets
+		String[] tableauString = new String[3];
+		tableauString[0] = "Un";
+		tableauString[1] = "Deux";
+		tableauString[2] = "Trois";
+	}
+}
