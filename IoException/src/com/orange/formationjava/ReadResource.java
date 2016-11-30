faa09243d1caefddb43e452b54cb10917e0c9d92
Philippe LEMORDANT <Philippe LEMORDANT>
2016-11-30 19:44:49 +0100

    projet IoException

--- IoException/src/com/orange/formationjava/ReadResource.java
+++ IoException/src/com/orange/formationjava/ReadResource.java
@@ -2,7 +2,6 @@ package com.orange.formationjava;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ReadResource {
