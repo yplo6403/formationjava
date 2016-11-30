bce65b360c7596bb38e96bfa5540c6460ac0ead8
Philippe LEMORDANT <Philippe LEMORDANT>
2016-11-30 19:43:57 +0100

    projet IoException

---
+++ IoException/src/com/orange/formationjava/ReadResource.java
@@ -0,0 +1,33 @@
+package com.orange.formationjava;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReadResource {
+
+	/**
+	 * Opens a text file and display its content.
+	 *
+	 * @param filename
+	 *            the file name
+	 * @return true if file exists and can be printed
+	 */
+	public boolean openAndDisplayFile(String filename) {
+		System.out.println("--- File start ---");
+
+		ClassLoader classLoader = getClass().getClassLoader();
+		FileInputStream fstream = new FileInputStream(classLoader.getResource(filename).getFile());
+		BufferedReader reader = new BufferedReader(new InputStreamReader(fstream));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			System.out.println(line);
+		}
+		reader.close();
+
+		System.out.println("--- File End ---");
+		return true;
+	}
+
+}
