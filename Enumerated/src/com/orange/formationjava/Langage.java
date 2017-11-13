361d7f9570b30519d6c7536169cb9419e4e3d1f8
Philippe LEMORDANT <lemordant.philippe@gmail.com>
2017-11-13 16:13:10 +0100

    Update for training november 2017

--- Enumerated/src/com/orange/formationjava/Langage.java
+++ Enumerated/src/com/orange/formationjava/Langage.java
@@ -2,6 +2,25 @@ package com.orange.formationjava;
 
 public enum Langage {
 
-	JAVA, CPP, PHP;
+	JAVA(0), CPP(1), PHP(2);
 
+	/**
+	 * Constructor
+	 * 
+	 * @param value
+	 *            the value associated to the name for persistence purpose
+	 */
+	private Langage(int value) {
+		// TODO 1 please complete
+	}
+	
+	public int toInt() {
+		// TODO 2 please fix and complete
+		return ?;
+	}
+
+	public static Langage valueOf(int value) {
+		// TODO 3 please fix and complete
+		return JAVA;
+	}
 }
